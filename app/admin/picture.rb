ca5e934c5715a5b1bdc7d8f194f4f58c9775fbee
Alexandra Sos <SOSfaceKILLA@Macintosh-78.local>
2014-06-25 17:51:00 -0700

    album working

--- app/admin/picture.rb
+++ app/admin/picture.rb
@@ -1,4 +1,6 @@
 ActiveAdmin.register Picture do
+
+	permit_params :album_id, :avatar_file_name, :avatar_content_type, :avatar_file_size, :avatar_updated_at
 	show do |ad|
       attributes_table do
 	  image_tag(ad.avatar.url(:thumb))
--- app/models/picture.rb
+++ app/models/picture.rb
@@ -2,5 +2,5 @@ class Picture < ActiveRecord::Base
   has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
   validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
   
-   belongs_to :album, :class_name => "Album", :foreign_key => "album_id"
+   belongs_to :album
 end
