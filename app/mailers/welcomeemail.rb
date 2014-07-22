9085a65abfb52bc45860be39508ff4d353561486
Alexandra Sos <SOSfaceKILLA@macintosh-78.home>
2014-07-21 23:00:48 -0700

    mailer

--- app/mailers/welcomeemail.rb
+++ app/mailers/welcomeemail.rb
@@ -4,10 +4,10 @@ class WelcomeMail < ActionMailer::Base
   def welcome(guest) # wizard is going to be a var that stores whichever email is passed in via the form
 
   	@email = guest # Here, we set an instance variable that matches wizard (we use an instance variable because we're going to refer to this in the email templates)
-  	mail(to: @alexsos@laguestlist.com, subject: "Hey") # Set up your to and subject fields!
+  	mail(to: alexsos@laguestlist.com, subject: "Hey") # Set up your to and subject fields!
 
   end
 
 
 
-end
+en
